1b9fe6cc3c93d9696ff3fe48744bfbc893efcbe0
yali-yl <1359320423@qq.com>
2021-04-16 13:33:01 +0800

    init ggshop

---
+++ src/filters/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import moment from 'moment'
+
+// 自定义过滤器
+// Vue.filter('date-format', function (value) {
+//   return moment(value).format('YYYY-MM-DD HH:mm:ss')
+// })
+// 或者
+Vue.filter('date-format', function (value, formatStr='YYYY-MM-DD HH:mm:ss') {
+  return moment(value).format(formatStr)
+})
+
